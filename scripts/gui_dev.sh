05a719bbf72779fc044ea224c878b478386d8467
Tomer Shalev <tshalev@proofpoint.com>
2024-04-16 10:53:46 +0000

    Add support for development mode without margins
    In development mode, we may want to print many times without wasting precious
    label. Setting DEV_MODE_NO_MARGINS=1 as environment variable will now print a
    short dot char (in GUI app), and without any margin.

--- scripts/gui_dev.sh
+++ scripts/gui_dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-    DEV_MODE_NO_MARGINS=$DEV_MODE_NO_MARGINS \
+    LABELLE_DEV_MODE_NO_MARGINS=$LABELLE_DEV_MODE_NO_MARGINS \
     LABELLE_VERBOSE=$LABELLE_VERBOSE \
         labelle-gui -v;
     sleep 1
--- src/labelle/lib/env_config.py
+++ src/labelle/lib/env_config.py
@@ -7,7 +7,7 @@ def is_env_var_true(env_var: str) -> bool:
 
 
 def is_dev_mode_no_margins():
-    return is_env_var_true("DEV_MODE_NO_MARGINS")
+    return is_env_var_true("LABELLE_DEV_MODE_NO_MARGINS")
 
 
 def is_verbose_env_vars() -> bool:
